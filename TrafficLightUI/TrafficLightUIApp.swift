c651bbb1750ec55c965228a66ca57dd59d44d1b8
Artem Shchegolev <mobilfree@mail.ru>
2021-10-26 11:43:03 +0300

    Initial Commit

---
+++ TrafficLightUI/TrafficLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightUIApp.swift
+//  TrafficLightUI
+//
+//  Created by Artem on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
