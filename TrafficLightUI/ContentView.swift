c851414f5af962b017e27e702e48e82698876bf0
Artem Shchegolev <mobilfree@mail.ru>
2021-10-27 21:03:16 +0300

    setup logic

--- TrafficLightUI/ContentView.swift
+++ TrafficLightUI/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var buttonText = "START"
+    @State private var redLamp = FlashLamp(color: .red)
+    @State private var yellowLamp = FlashLamp(color: .yellow)
+    @State private var greenLamp = FlashLamp(color: .green)
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            VStack {
+                redLamp
+                yellowLamp
+                greenLamp
+                Spacer()
+                nextButton
+            }
             .padding()
+        }
+    }
+    
+    private var nextButton: some View {
+        Button(action: { buttonPressed() }) {
+            Text(buttonText)
+                .frame(width: 150, height: 50)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(20)
+                .font(.title)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.white, lineWidth: 4)
+                    )
+        }
+        .padding()
+    }
+    
+    private func buttonPressed() {
+        buttonText = "NEXT"
+        
+        if redLamp.lightOn {
+            redLamp.lightOn = false
+            yellowLamp.lightOn = true
+        } else if yellowLamp.lightOn {
+            yellowLamp.lightOn = false
+            greenLamp.lightOn = true
+        } else if greenLamp.lightOn {
+            greenLamp.lightOn = false
+            redLamp.lightOn = true
+        } else {
+            redLamp.lightOn = true
+        }
     }
 }
 
